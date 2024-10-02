8b0a481ad8229ade45080fa119e9ce1ed5b979f2
Aetous <Aetous@proton.me>
2024-10-02 00:57:01 -0700

    Added blogsite to TTS ad network

---
+++ docs/js/AdvertisementLib.js
@@ -0,0 +1,22 @@
+console.log("Hello World");
+
+fetch("https://tailtwiststudios.github.io/TTS-VRC-Public-Resources/universal/adMetadata.json")
+    .then(function (response) {
+        //wait for the promise to be fufilled
+        return response.json();
+    })
+    .then(function(data) {
+        console.log(data);
+        let adMetadata = data.adMetadata;
+
+        for (let i = 0; i < 3; i++) {
+            let imageElement = document.getElementById(`ad-image-${(i+1)}`);
+            imageElement.alt = adMetadata[i].imageAltText;
+
+            let linkElement = document.getElementById(`ad-link-${(i+1)}`);
+            linkElement.href = adMetadata[i].linkDestination;
+        }
+    })
+    .catch(function (err) {
+        console.log(err);
+    });
